3dc8c4dfb80a860a2230c6be9720d51837761a9c
Sumith Damodaran <sumithpd@gmail.com>
2023-01-05 14:56:12 +0000

    adding fetch and scrolling

--- src/App.js
+++ src/App.js
@@ -2,32 +2,50 @@ import React, { Component } from 'react'
 import './App.css'
 import 'tachyons'
 import CardList from './CardList'
-import { robots } from './robots'
+// import { robots } from './robots'
 import SearchBox from './SearchBox'
+import Scroll from './Scroll'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: '',
     }
   }
-  onSearchChange =(event)=>{
-    this.setState({searchfield:event.target.value});
-   
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        return response.json()
+      })
+      .then((users) => {
+        this.setState({ robots: users })
+      })
+  }
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
   }
   render() {
-    const filteredRobots = this.state.robots.filter(robots =>{
-      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    const filteredRobots = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase())
     })
-    return (
-      <div className="tc">
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    )
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+          
+        </div>
+      )
+    }
   }
 }
 
